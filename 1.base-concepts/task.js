fecc07b9e55aca49cde8251917a39d009369f099
VanyaPonomarev <vanya.ponomaryov@gmail.com>
2021-11-09 12:03:11 +0300

    Honework checked and done

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,38 @@
+'use strict'
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
+  let arr = [];
+  let d = b ** 2 - 4 * a * c;
+  if (d > 0) {
+    arr[0] = (- b + Math.sqrt(d)) / (2 * a);
+    arr[1] = (- b - Math.sqrt(d)) / (2 * a);
+  }
+  else if (d == 0) {
+    arr[0] = -b / (2 * a);
+  }
+  else if (d < 0) {
+    arr;
+  }
   return arr; // array
 }
 
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
+  if (isNaN(percent)) {
+    return `Параметр "Процентная ставка" содержит неправильное значение "${percent}"`
+  } else if (isNaN(contribution)) {
+    return `Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`
+  } else if (isNaN(amount)) {
+    return `Параметр "Общая стоимость" содержит неправильное значение "${amount}"`
+  }
+  let month = new Date().getMonth();
+  let year = new Date().getFullYear();
+  let numberOfyears = date.getFullYear() - year;
+  let n = numberOFyears * 12 - month + date.getMonth();
+  let P = (percent / 100) / 12;
+  let S = amount - contribution;
+  let payment = S * (P + (P / (((1 + P) ** n) - 1)))
+  let totalAmount = (payment * n).toFixed(2);
 
-  // код для задачи №2 писать здесь
-
-  return totalAmount;
+  return Number(totalAmount);
 }
